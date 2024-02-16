aeebc6a3e0118114a42add7151708e22ad5d01a6
rociopalma75 <rociopalma75@gmail.com>
2024-02-15 21:09:55 -0300

    Se agregaron detalles como limpiar consola

--- clase3_ejercicio1.py
+++ clase3_ejercicio1.py
@@ -1,3 +1,5 @@
+from os import system
+
 def suma(num1, num2):
     return num1+num2
 
@@ -10,14 +12,17 @@ while i == 1 :
     num1 = float(input("Ingrese el primer numero: "))
     num2 = float(input("Ingrese el segundo numero: "))
     
-    eleccion = input("Ingresar la accion \n\tA-> suma\n\tB-> resta\n\tRespuesta: ")
+    eleccion = input("Ingresar la accion \n\tA-> suma \n\tB-> resta \n\tRespuesta: ")
 
     if eleccion == "A" or eleccion == 'a':
         print(f'El resultado de {num1} + {num2} = {suma(num1,num2)}')
     elif eleccion == 'B' or eleccion == 'b':
-        print(f'El resultado de {num1} - {num2} = {suma(num1,num2)}')
+        print(f'El resultado de {num1} - {num2} = {resta(num1,num2)}')
     else:
         print("Ingreso una opcion incorrecta")
 
-    respuestaUsuario = input("Desea seguir operando: \n\tIngrese 0 -> No\n\tIngrese 1 -> SI\n\tRespuesta: ")
+    
+    respuestaUsuario = input("Desea seguir operando: \n\tIngrese 0 -> No \n\tIngrese 1 -> SI \n\tRespuesta: ")
     i = int(respuestaUsuario)
+
+    system("cls")
