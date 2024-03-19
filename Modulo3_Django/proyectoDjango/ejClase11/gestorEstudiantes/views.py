deff2752d238993dda17955c2a6b5cf3e7a75baa
rociopalma75 <rociopalma75@gmail.com>
2024-03-19 20:56:32 -0300

    Ejercicio de la clase 12

--- Modulo3_Django/proyectoDjango/ejClase11/gestorEstudiantes/views.py
+++ Modulo3_Django/proyectoDjango/ejClase11/gestorEstudiantes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Estudiante
-
+from django.template import Template, Context
 # Create your views here.
 
 # Create your views here.
@@ -11,25 +12,50 @@ def createEstudiante(request, nombre, apellido, edad, nota):
         edad = edad,
         nota = nota
     )
-    return render(request, 'nuevo_estudiante.html', {'estudiante': nuevoEstudiante})
+    ruta = "C:\\Users\\rocio\\OneDrive\\Documentos\\Alkemy_python\\Modulo3_Django\\proyectoDjango\\ejClase11\\gestorEstudiantes\\templates\\nuevo_estudiante.html"
+    doc_externo = open(ruta)
+    plantilla = Template(doc_externo.read())
+    doc_externo.close()
+    contexto = Context({"estudiante": nuevoEstudiante})
+    return HttpResponse(plantilla.render(contexto))
 
 def readEstudiantes(request):
     estudiantes = Estudiante.objects.all()
-    return render(request, 'lista_estudiantes.html', {'estudiantes' : estudiantes})
+    ruta = "C:\\Users\\rocio\\OneDrive\\Documentos\\Alkemy_python\\Modulo3_Django\\proyectoDjango\\ejClase11\\gestorEstudiantes\\templates\\lista_estudiantes.html"
+    doc_externo = open(ruta)
+    plantilla = Template(doc_externo.read())
+    doc_externo.close()
+    contexto = Context({"estudiantes":estudiantes})
+    return HttpResponse(plantilla.render(contexto))
 
 def updateNota(request, id, nota):
     estudiante = Estudiante.objects.get(id = id)
     estudiante.nota = nota
     estudiante.save()
     estudiantes = Estudiante.objects.all()
-    return render(request, 'lista_estudiantes.html', {'estudiantes': estudiantes})
+    ruta = "C:\\Users\\rocio\\OneDrive\\Documentos\\Alkemy_python\\Modulo3_Django\\proyectoDjango\\ejClase11\\gestorEstudiantes\\templates\\lista_estudiantes.html"
+    doc_externo = open(ruta)
+    plantilla = Template(doc_externo.read())
+    doc_externo.close()
+    contexto = Context({"estudiantes":estudiantes})
+    return HttpResponse(plantilla.render(contexto))
 
 def filterEstudiante(request, edad):
     estudiantes = Estudiante.objects.filter(edad__gte = edad)
-    return render(request, 'lista_estudiantes.html',{'estudiantes':estudiantes})
+    ruta = "C:\\Users\\rocio\\OneDrive\\Documentos\\Alkemy_python\\Modulo3_Django\\proyectoDjango\\ejClase11\\gestorEstudiantes\\templates\\lista_estudiantes.html"
+    doc_externo = open(ruta)
+    plantilla = Template(doc_externo.read())
+    doc_externo.close()
+    contexto = Context({"estudiantes":estudiantes})
+    return HttpResponse(plantilla.render(contexto))
 
 def deleteEstudiante(request, id):
     estudiante = Estudiante.objects.get(id = id)
     estudiante.delete()
     estudiantes = Estudiante.objects.all()
-    return render(request, 'lista_estudiantes.html', {'estudiantes': estudiantes})
+    ruta = "C:\\Users\\rocio\\OneDrive\\Documentos\\Alkemy_python\\Modulo3_Django\\proyectoDjango\\ejClase11\\gestorEstudiantes\\templates\\lista_estudiantes.html"
+    doc_externo = open(ruta)
+    plantilla = Template(doc_externo.read())
+    doc_externo.close()
+    contexto = Context({"estudiantes":estudiantes})
+    return HttpResponse(plantilla.render(contexto))
